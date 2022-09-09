94d3e56861315d86d89f44f4de731c6388bd6786
Elijah Ndibe <elijahndibe@gmail.com>
2022-09-09 07:00:34 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ *
+ *  main - Entry point
+ *
+ *  Description: "a C program that prints exactly \"Programming is like building a multilingual puzzle, followed by a new line" 
+ *
+ *  Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
